df3b06b1b8f8195be9233b0a558bd09d12354cdb
pedrocvm <pedrocvm2@gmail.com>
2021-04-30 20:12:35 -0400

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Header />
+    <v-main>
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import Vue from 'vue';
+import Header from '@/components/Header.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+
+  }),
+});
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/views/Register.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="registerWrapper">
+    <v-card>
+      <v-form>
+        <v-row>
+          <v-col>
+            <v-text-field
+              placeholder="insira o nome do medicamento"
+              outlined
+              label="Nome"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              placeholder="Insira a quantidade de medicamento a ser ministrada ao dia "
+              outlined
+              label="Posologia"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <v-select
+              label="Período"
+              outlined
+              hide-details="auto"
+              :items="dayPeriods"
+              :item-text="'label'"
+              :item-value="'value'"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col>
+            <v-text-field
+              outlined
+              label="Dosagem"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <v-checkbox v-model="uniqueDose" label="Dose Única"></v-checkbox>
+          </v-col>
+
+          <v-col v-if="!uniqueDose">
+            <v-text-field
+              outlined
+              label="Intervalo"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row> </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea outlined label="Descrição"></v-textarea>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="actionWrapper">
+            <v-btn color="warning" elevation="2">Enviar</v-btn>
+            <v-btn color="primary" elevation="2">Limpar</v-btn>
+          </v-col>
+        </v-row>
+
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dayPeriods: [
+        {
+          label: 'Manhã',
+          value: 'MORNING',
+        },
+        {
+          label: 'Tarde',
+          value: 'AFTERNOON',
+        },
+        {
+          label: 'Noite',
+          value: 'NIGHT',
+        },
+      ],
+      uniqueDose: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../sass/master';
+
+.registerWrapper {
+  height: calc(100vh - 70px);
+  padding: 30px;
+
+  .v-card {
+    padding: 30px;
+  }
+
+  .actionWrapper {
+    @include flexbox(row, center, center);
+    gap: 30px;
+  }
+}
+</style>
